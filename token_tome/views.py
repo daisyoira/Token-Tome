a830e48fde3a969060b8600b7626999717798521
daisy-dotcom <61582926+daisy-dotcom@users.noreply.github.com>
2024-03-24 21:28:30 +0800

    added easily readable error codes to views.py

--- token_tome/views.py
+++ token_tome/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from token_tome.models import User
 from token_tome.serializers import UserSerializer
 # Create your views here.
@@ -12,6 +17,7 @@ def index(request):
 
 
 @csrf_exempt
+@api_view
 def user_list(request):
     """
     List all users, or create a new user.
@@ -26,11 +32,14 @@ def user_list(request):
         serializer = UserSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return JsonResponse(serializer.data,
+                                status=status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
 
 
 @csrf_exempt
+@api_view
 def single_user(request, username):
     """
     Retrieve, update or delete a user's information.
@@ -39,7 +48,7 @@ def single_user(request, username):
     try:
         user = User.objects.get(username=username)
     except User.DoesNotExist:
-        return HttpResponse(status=404)
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = UserSerializer(user)
@@ -54,8 +63,9 @@ def single_user(request, username):
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+        return JsonResponse(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         user.delete()
-        return HttpResponse(status=204)
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
