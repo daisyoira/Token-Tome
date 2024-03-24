c6d9c5eeca964883a0ae607bbb2f0955482ea945
daisy-dotcom <61582926+daisy-dotcom@users.noreply.github.com>
2024-03-24 20:11:46 +0800

    updated User model to reject blank fields

--- token_tome/models.py
+++ token_tome/models.py
@@ -2,6 +2,8 @@ from django.db import models
 # Create your models here.
 
 
+# field: blank=True, the field is allowed
+# to be blank; default is False
 class User(models.Model):
-    username = models.CharField(max_length=200)
-    token = models.CharField(max_length=100)
+    username = models.CharField(max_length=200, blank=False)
+    token = models.CharField(max_length=100, blank=False)
--- token_tome/serializers.py
+++ token_tome/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from token_tome.models import User
 
 
-class UserSerilaizer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'token']
