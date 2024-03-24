a51112d7f91e19e84731e411554e82c43f9b6adb
daisy-dotcom <61582926+daisy-dotcom@users.noreply.github.com>
2024-03-24 20:19:07 +0800

    added some test code to token_tome.tests

--- token_tome/tests.py
+++ token_tome/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+from token_tome.models import User
+from token_tome.serializers import UserSerializer
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+
+snippet = User(username='test_1', token='121ew')
+# saves to db
+snippet.save()
+
+serializer = UserSerializer(snippet)
+print(serializer.data)
+
+content = JSONRenderer().render(serializer.data)
+print(content)
