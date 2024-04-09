0c63dd014bfd50b49af07a92e5980bc658913b80
daisyoira <118959565+daisyoira@users.noreply.github.com>
2024-04-10 01:29:37 +0800

    autogenerate token when a new name is posted using python's secrets module

--- token_tome/models.py
+++ token_tome/models.py
@@ -1,10 +1,12 @@
+import secrets
+
 from django.db import models
 # Create your models here.
 
 
-# field: blank=True, the field is allowed
-# to be blank; default is False
 class Student(models.Model):
     name = models.CharField(max_length=200)
     token = models.CharField(max_length=100,
-                             unique=True)
+                             unique=True,
+                             editable=False,
+                             default=secrets.token_urlsafe(7))
