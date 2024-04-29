30f1bf20f7f2b3f69294857b7ae55c360c5823b9
daisyoira <118959565+daisyoira@users.noreply.github.com>
2024-04-29 21:15:34 +0800

    added file download url and url to media files

--- token_tome/urls.py
+++ token_tome/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
     path("", views.FileUploadFormView.as_view()),
     path("create-student", views.StudentCreateView.as_view()),
+    re_path("download/(?P<file_name>[-a-zA-Z0-9_]+)\\Z", views.FileDownloadView.as_view(), name='download'),
     path("student-token/<int:pk>", views.StudentDetailView.as_view(), name='student-token'),
     path("api", views.api_root, name="index"),
     path("api/students/", views.StudentList.as_view(), name="student-list"),
@@ -15,4 +18,5 @@ urlpatterns = [
     path("api/users/<slug:pk>", views.UserDetail.as_view(), name="user-detail"),
     path('api/users/<slug:pk>/highlight/', views.UserHighlight.as_view(), name="user-highlight"),
     path('api/upload/', views.FileUploadView.as_view(), name='file_upload')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
