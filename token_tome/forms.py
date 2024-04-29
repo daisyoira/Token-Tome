5345b355634f257e4753a15a936b6816aec442c3
daisyoira <118959565+daisyoira@users.noreply.github.com>
2024-04-29 15:02:23 +0800

    updated FileUploadForm in forms.py to use for the frontend instead of a model based CreateView

--- token_tome/forms.py
+++ token_tome/forms.py
@@ -1,6 +1,24 @@
 from django import forms
+from token_tome.models import Student, File
 
 
 class FileUploadForm(forms.Form):
-    student = forms.ChoiceField()
-    file = forms.FileField()
+
+    file = forms.FileField(widget=forms.FileInput(attrs={'accept': 'application/pdf'}),
+                           required=True)
+
+    # to_field_name - specifies column to use as value
+    student = forms.ModelChoiceField(queryset=Student.objects.all(),
+                                     required=True,
+                                     to_field_name="token")
+
+    def __init__(self, *args, **kwargs):
+        super(FileUploadForm, self).__init__(*args, **kwargs)
+
+        # use student name as the label
+        self.fields['student'].label_from_instance = self.label_from_instance
+
+    @staticmethod
+    def label_from_instance(student):
+        return student.name
+
