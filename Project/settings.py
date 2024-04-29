faa4ef30079b5f22b231b6d914b297c354ffd5cb
daisyoira <118959565+daisyoira@users.noreply.github.com>
2024-04-29 15:10:30 +0800

    updated static and media urls in settings.py

--- Project/settings.py
+++ Project/settings.py
@@ -123,10 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / "static",
+#    "/var/www/static/",
+#]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -142,6 +145,4 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 
-MEDIA_URL = '/files/'
-MEDIA_ROOT = BASE_DIR / 'files'
 
