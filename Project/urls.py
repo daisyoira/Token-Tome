82fc38dc9a7d5696a7c9f579bfe0592f9b13f5d8
daisyoira <118959565+daisyoira@users.noreply.github.com>
2024-05-08 22:43:49 +0800

    added the api-auth path again

--- Project/urls.py
+++ Project/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('', include("token_tome.urls")),
     path('token_tome/', include("token_tome.urls")),
     path('admin/', admin.site.urls),
-    #path('api-auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
